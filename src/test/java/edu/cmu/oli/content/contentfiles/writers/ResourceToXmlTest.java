94057723145109b4bd0ac2296597a2d813e9c029
Raphael Gachuhi <rgachuhi@cmu.edu>
2019-05-22 17:44:15 -0400

    RG

--- src/test/java/edu/cmu/oli/content/contentfiles/writers/ResourceToXmlTest.java
+++ src/test/java/edu/cmu/oli/content/contentfiles/writers/ResourceToXmlTest.java
@@ -8,6 +8,8 @@ import com.google.gson.JsonParser;
 import edu.cmu.oli.content.configuration.Configurations;
 import edu.cmu.oli.content.configuration.Configurator;
 import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -81,6 +83,28 @@ public class ResourceToXmlTest {
 
     // Make sure that there are no referenes to audio/mpeg
     assertTrue(xml.indexOf("type=\"audio/mpeg") == -1);
+  }
+
+  /**
+   * This test ensures that responses are correctly aligned to their inputs
+   * when a multipart question is transformed from JSON to XML.
+   */
+  @Test
+  public void testPreservationOfResponseInputsOrder() throws JDOMException, IOException {
+
+    Path path = Paths.get(ResourceToXmlTest.class.getResource("./multi-part-assessment.json").getFile());
+    String json = new String(Files.readAllBytes(path));
+
+    final ResourceToXml toXml = new ResourceToXml();
+    final Configurations config = loadConfiguration();
+
+    toXml.setConfig(config);
+    String xml = toXml.resourceToXml("x-oli-inline-assessment", json);
+    xml = xml.replaceAll("\n", "");
+//    System.out.println(xml);
+
+    // Verify correct alignment between match and input
+    assertTrue(xml.indexOf("match=\"sm_under\" score=\"1\" input=\"s5\"") > 1);
   }
 
 }
