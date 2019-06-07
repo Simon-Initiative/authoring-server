be09a84e4ea1e1ef326ca2f842538b36a852d3c1
Eli Knebel <bitbldr@gmail.com>
2019-06-07 12:56:31 -0400

    change query timeout to 15 mins

--- src/main/java/edu/cmu/oli/content/reportingdb/DbConnector.java
+++ src/main/java/edu/cmu/oli/content/reportingdb/DbConnector.java
@@ -51,8 +51,8 @@ public class DbConnector {
                 Statement statement = connection.createStatement()) {
             Class.forName("com.mysql.jdbc.Driver");
 
-            // Terminate if query takes longer than 4 minutes
-            statement.setQueryTimeout(60 * 4);
+            // Terminate if query takes longer than 15 minutes
+            statement.setQueryTimeout(60 * 15);
             log.info("Executing query: " + query);
 
             ResultSet result = statement.executeQuery(query);
