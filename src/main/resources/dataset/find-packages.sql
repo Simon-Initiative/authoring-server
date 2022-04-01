9b762a03ba983f2ea970bfc14c021dfbb38620b1
Eli Knebel <eliknebel@gmail.com>
2022-04-01 09:49:42 -0400

    one variable per line in sql substitution

--- src/main/resources/dataset/find-packages.sql
+++ src/main/resources/dataset/find-packages.sql
@@ -1,6 +1,8 @@
 # Query finds package guids for all previous versions of a package
 # identified by the package id and version
+# substitution code assumes one variable per line
 select distinct guid, id, version
 from content.package 
-where id = :packageId and version <= :packageVersion
+where id = :packageId
+and version <= :packageVersion
 order by date_created desc;
